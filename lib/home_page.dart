97e15c9bdbf1512ecdea9d19f53bb5d7f5ea0907
Nargiza0987 <kulm1987@gmail.com>
2023-10-23 10:10:44 +0300

    playing with animations

---
+++ lib/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Home_page extends StatefulWidget {
+  const Home_page({super.key});
+
+  @override
+  State<Home_page> createState() => _Home_pageState();
+}
+
+class _Home_pageState extends State<Home_page> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _radiusAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // animation controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    )..forward();
+
+    // rotation animation
+    _rotationAnimation = Tween(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    //radius animation -> from circle to square
+    _rotationAnimation = Tween(begin: 450.0, end: 10.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+
+    // make animation go back forth
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[300],
+      body: Center(
+        child: Stack(
+          children: [
+            // biggest object
+            Container(
+              width: 225,
+              height: 225,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple[400],
+              ),
+            ),
+
+            // 2nd biggest object
+
+            // 3rd biggest object
+          ],
+        ),
+      ),
+    );
+  }
+}
